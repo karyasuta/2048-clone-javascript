91359bd16c9e34cb39f8e9c353321604777651c8
karyasuta <81769360+karyasuta@users.noreply.github.com>
2023-06-08 21:00:51 +0800

    Add some helper functions

--- script.js
+++ script.js
@@ -15,15 +15,31 @@ function setupInput() {
 async function handleInput(e) {
   switch (e.key) {
     case 'ArrowUp':
+      if (!canMoveUp()) {
+        setupInput();
+        return;
+      }
       await moveUp();
       break;
     case 'ArrowDown':
+      if (!canMoveDown()) {
+        setupInput();
+        return;
+      }
       await moveDown();
       break;
     case 'ArrowLeft':
+      if (!canMoveLeft()) {
+        setupInput();
+        return;
+      }
       await moveLeft();
       break;
     case 'ArrowRight':
+      if (!canMoveRight()) {
+        setupInput();
+        return;
+      }
       await moveRight();
       break;
     default:
@@ -33,6 +49,15 @@ async function handleInput(e) {
 
   grid.cells.forEach((cell) => cell.mergeTiles());
 
+  const newTile = new Tile(gameBoard);
+  grid.randomEmptyCell().tile = newTile;
+
+  if (!canMoveUp() && !canMoveDown() && !canMoveLeft() && !canMoveRight()) {
+    newTile.waitForTransition(true).then(() => {
+      alert('You lose');
+    });
+    return;
+  }
   setupInput();
 }
 
@@ -78,4 +103,28 @@ function slideTiles(cells) {
       return promises;
     })
   );
+}
+
+function canMoveUp() {
+  return canMove(grid.cellsByColumn);
+}
+function canMoveDown() {
+  return canMove(grid.cellsByColumn.map((column) => [...column].reverse()));
+}
+function canMoveRight() {
+  return canMove(grid.cellsByRow);
+}
+function canMoveLeft() {
+  return canMove(grid.cellsByRow.map((row) => [...row].reverse()));
+}
+
+function canMove(cells) {
+  return cells.some((group) => {
+    return group.some((cell, index) => {
+      if (index === 0) return false;
+      if (cell.tile === null) return false;
+      const moveToCell = group[index - 1];
+      return moveToCell.canAccept(cell.tile);
+    });
+  });
 }
